23a15526abbbb232eaa23b32cd318f9db308f28a
Animesh Samanta <animeshsamanta1919@gmail.com>
2023-10-28 21:27:39 +0530

    First commit

---
+++ Number_recognisation.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 28 21:14:09 2023
+
+@author: Animesh
+"""
+
+pip install tensorflow
+
+
+import tensorflow 
+from tensorflow import keras
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense,Flatten
+
+(X_train,y_train),(X_test,y_test) = keras.datasets.mnist.load_data()
+
+X_test.shape
+y_train.shape
+
+y_train.shape
+
+plt.imshow(X_train[4])
+
+X_train = X_train/255
+X_test = X_test/255
+
+X_train[0]
+
+
+model = Sequential()
+
+model.add(Flatten(input_shape=(28,28)))
+model.add(Dense(128,activation='relu'))
+model.add(Dense(32,activation='relu'))
+model.add(Dense(10,activation='softmax'))
+
+model.summary()
+
+model.compile(loss='sparse_categorical_crossentropy',optimizer='Adam',metrics=['accuracy'])
+
+history = model.fit(X_train,y_train,epochs=25,validation_split=0.2)
+
+
+y_prob = model.predict(X_test)
+
+y_prob.argmax(axis=1)
+
+y_pred = y_prob.argmax(axis=1)
+
+from sklearn.metrics import accuracy_score
+accuracy_score(y_test,y_pred)
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+
+plt.imshow(X_test[4])
+
+model.predict(X_test[4].reshape(1,28,28)).argmax(axis=1)
+
+
+
+
+
+
